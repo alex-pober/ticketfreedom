b65d6f93b334c73fa9b97f19f040dd78627626b7
Alex <alex.pober.95@gmail.com>
2024-03-18 14:09:40 -0700

    added all courts location to drop down and made it scrollable

--- components/ComboBox.tsx
+++ components/ComboBox.tsx
@@ -17,6 +17,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { ScrollArea } from "./ui/scroll-area"
 
 const frameworks = [
   {
@@ -32,12 +33,152 @@ const frameworks = [
     label: "Bellflower Courthouse",
   },
   {
+    value: "Beverly Hills Courthouse",
+    label: "Beverly Hills Courthouse",
+  },
+  {
     value: "Burbank Courthouse",
     label: "Burbank Courthouse",
   },
   {
-    value: "Calabases Courthouse",
-    label: "Calabases Courthouse",
+    value: "Calabasas Courthouse",
+    label: "Calabasas Courthouse",
+  },
+  {
+    value: "Catalina Courthouse",
+    label: "Catalina Courthouse",
+  },
+  {
+    value: "Central Arraignment Courthouse",
+    label: "Central Arraignment Courthouse",
+  },
+  {
+    value: "Chatsworth Courthouse",
+    label: "Chatsworth Courthouse",
+  },
+  {
+    value: "Compton Courthouse",
+    label: "Compton Courthouse",
+  },
+  {
+    value: "Culver City Courthouse",
+    label: "Culver City Courthouse",
+  },
+  {
+    value: "Downey Courthouse",
+    label: "Downey Courthouse",
+  },
+  {
+    value: "East Los Angeles Courthouse",
+    label: "East Los Angeles Courthouse",
+  },
+  {
+    value: "El Monte Courthouse",
+    label: "El Monte Courthouse",
+  },
+  {
+    value: "Glendale Courthouse",
+    label: "Glendale Courthouse",
+  },
+  {
+    value: "Governor George Deukmejian Courthouse",
+    label: "Governor George Deukmejian Courthouse",
+  },
+  {
+    value: "Hollywood Courthouse",
+    label: "Hollywood Courthouse",
+  },
+  {
+    value: "Huntington Park Courthouse",
+    label: "Huntington Park Courthouse",
+  },
+  {
+    value: "Inglewood Courthouse",
+    label: "Inglewood Courthouse",
+  },
+  {
+    value: "Lynwood Regional Justice Center",
+    label: "Lynwood Regional Justice Center",
+  },
+  {
+    value: "Malibu Courthouse",
+    label: "Malibu Courthouse",
+  },
+  {
+    value: "Metropolitan Courthouse",
+    label: "Metropolitan Courthouse",
+  },
+  {
+    value: "Michael Antonovich Antelope Valley Courthouse",
+    label: "Michael Antonovich Antelope Valley Courthouse",
+  },
+  {
+    value: "Monrovia Courthouse",
+    label: "Monrovia Courthouse",
+  },
+  {
+    value: "Pasadena Courthouse",
+    label: "Pasadena Courthouse",
+  },
+  {
+    value: "Pomona Courthouse North",
+    label: "Pomona Courthouse North",
+  },
+  {
+    value: "Redondo Beach Courthouse",
+    label: "Redondo Beach Courthouse",
+  },
+  {
+    value: "San Fernando Courthouse",
+    label: "San Fernando Courthouse",
+  },
+  {
+    value: "San Pedro Courthouse",
+    label: "San Pedro Courthouse",
+  },
+  {
+    value: "Santa Clarita Courthouse",
+    label: "Santa Clarita Courthouse",
+  },
+  {
+    value: "Santa Monica Courthouse",
+    label: "Santa Monica Courthouse",
+  },
+  {
+    value: "South Gate Courthouse",
+    label: "South Gate Courthouse",
+  },
+  {
+    value: "Torrance Courthouse",
+    label: "Torrance Courthouse",
+  },
+  {
+    value: "Van Nuys Courthouse West",
+    label: "Van Nuys Courthouse West",
+  },
+  {
+    value: "West Covina Courthouse",
+    label: "West Covina Courthouse",
+  },
+  {
+    value: "West Los Angeles Courthouse",
+    label: "West Los Angeles Courthouse",
+  },
+  {
+    value: "West Valley Ticket Payment Office",
+    label: "West Valley Ticket Payment Office",
+  },
+  {
+    value: "Whittier Courthouse",
+    label: "Whittier Courthouse",
+  },
+  {
+    value: "Orange County Courthouse",
+    label: "Orange County Courthouse",
+  },
+  {
+    value: "Riverside Courthouse",
+    label: "Riverside Courthouse",
   },
 ]
 
@@ -46,7 +187,7 @@ export function ComboboxDemo() {
   const [value, setValue] = useState("")
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover modal open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
           variant="outline"
@@ -60,31 +201,33 @@ export function ComboboxDemo() {
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[300px] p-0">
+      <PopoverContent className="w-[300px] p-0 h-60">
         <Command>
           <CommandInput placeholder="Search court..." />
           <CommandEmpty>No court found.</CommandEmpty>
-          <CommandGroup>
-            {frameworks.map((framework) => (
-              <CommandItem
-                key={framework.value}
-                value={framework.value}
-                onSelect={(currentValue) => {
-                  console.log(framework)
-                  setValue(framework.value)
-                  setOpen(false)
-                }}
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    value === framework.value ? "opacity-100" : "opacity-0"
-                  )}
-                />
-                {framework.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
+          <ScrollArea>
+            <CommandGroup>
+              {frameworks.map((framework) => (
+                <CommandItem
+                  key={framework.value}
+                  value={framework.value}
+                  onSelect={(currentValue) => {
+                    console.log(framework)
+                    setValue(framework.value)
+                    setOpen(false)
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === framework.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {framework.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </ScrollArea>
         </Command>
       </PopoverContent>
     </Popover>
