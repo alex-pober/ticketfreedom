101faccee030f320578c3d8527ebed169eb3beab
Alex <alex.pober.95@gmail.com>
2024-03-22 16:10:07 -0700

    removed SVG from nextjs image as per their recommendation

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,15 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/CowzfwdSDEY
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ */
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { SheetTrigger, SheetContent, Sheet } from "@/components/ui/sheet";
 import { Separator } from "./ui/separator";
 import AuthButton from "./AuthButton";
-import LogoSvgWhite from "@/app/logo-new-white.svg";
+import LogoWhite from "@/app/LogoWhiteSvg";
 import { Badge } from "./ui/badge";
 import { ServicesDialog } from "./ServicesDialog";
 
@@ -12,7 +17,7 @@ export default function NavBar() {
   return (
     <div className="flex items-center justify-between min-w-full p-4 bg-black dark:bg-gray-800">
       <Link className="flex items-center gap-2" href="#">
-        <Image priority alt="logo image" src={LogoSvgWhite} width={200} height={60} />
+        <LogoWhite />
       </Link>
       <div className="flex gap-4">
         <ServicesDialog />
@@ -29,5 +34,44 @@ export default function NavBar() {
         </div>
       </div>
     </div>
+  );
+}
+
+function MenuIcon(props: any) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#ffd35c"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="3" x2="21" y1="6" y2="6" />
+      <line x1="3" x2="21" y1="12" y2="12" />
+      <line x1="3" x2="21" y1="18" y2="18" />
+    </svg>
+  );
+}
+
+function MountainIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+    </svg>
   );
 }
