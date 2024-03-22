c2f6fe4d2f1adffe58af247c0d8987aa8ceb549b
Alex <alex.pober.95@gmail.com>
2024-03-22 15:29:31 -0700

    metatags changed for the nextjs 14 app router style

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,66 @@
 import { GeistSans } from "geist/font/sans";
 import { cn } from "../lib/utils"
 import { fontSans } from "@/lib/fonts"
-import Head from "next/head";
 import "./globals.css";
+
+export const metadata = {
+  title: "Ticket Freedom - Los Angeles Traffic Ticket Lawyer",
+  description:
+    "Get your traffic tickets handled by a professional lawyer in Los Angeles. Fast, efficient, and reliable legal services to fight your traffic ticket violations. Save time, money, and stress with Ticket Freedom.",
+  keywords: [
+    "Traffic Ticket",
+    "Los Angeles Lawyer",
+    "Traffic Violation",
+    "Legal Services",
+    "Ticket Freedom",
+    "DUI Lawyer"
+  ],
+  metadataBase: new URL("https://www.ticketfreedom.com"),
+  openGraph: {
+    url: "https://www.ticketfreedom.com",
+    type: "website",
+    title: "Ticket Freedom - Los Angeles Traffic Ticket Lawyer",
+    description:
+     "Professional legal services in Los Angeles for fighting your traffic ticket violations. Contact us today to save on fines and avoid points on your license.",
+     images: [
+      {
+        url: "https://www.ticketfreedom.com/og-image.jpg",
+        width: 1120,
+        height: 676,
+        alt: "Buisness Card of Ticket Freedom"
+      }
+    ]
+  },
+  alternates: {
+    canonical: "https://www.ticketfreedom.com"
+  },
+  robots: {
+    index: true,
+    follow: true,
+    "max-image-preview": "large",
+    "max-snippet": -1,
+    "max-video-preview": -1,
+    googleBot: "index, follow"
+  },
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        type: "image/x-icon"
+      },
+    ],
+  },
+  shortcut: [
+    {
+      url: "/favicon.ico",
+      type: "image/x-icon"
+    }
+  ],
+}
 
 export default function RootLayout({children}) {
   return (
     <html lang="en" className={GeistSans.className}>
-         <Head>
-        <title>Ticket Freedom - Los Angeles Traffic Ticket Lawyer</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta charSet="utf-8" />
-        <meta name="description" content="Get your traffic tickets handled by a professional lawyer in Los Angeles. Fast, efficient, and reliable legal services to fight your traffic ticket violations. Save time, money, and stress with Ticket Freedom." />
-        <meta name="keywords" content="Traffic Ticket, Los Angeles Lawyer, Traffic Violation, Legal Services, Ticket Freedom" />
-        <meta name="author" content="Ticket Freedom" />
-
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.ticketfreedom.com/" />
-        <meta property="og:title" content="Ticket Freedom - Los Angeles Traffic Ticket Lawyer" />
-        <meta property="og:description" content="Professional legal services in Los Angeles for fighting your traffic ticket violations. Contact us today to save on fines and avoid points on your license." />
-        <meta property="og:image" content="https://www.ticketfreedom.com/og-image.jpg" />
-
-        {/* Add additional meta tags here as needed */}
-
-        {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <body className={cn(
           "min-h-screen bg-background font-sans subpixel-antialiased",
           fontSans.className
